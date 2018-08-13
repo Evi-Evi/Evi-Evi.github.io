4988219d2b7fff8b3755d282524f80656671a79e
Evi-ava <dedasertyk.fvhg@gmail.com>
2018-08-13 23:30:33 +0400

    sdfsf

---
+++ work3/js/main.js
@@ -0,0 +1,31 @@
+/*$(document).ready(function(){
+    
+   
+    
+    
+    $(window).on("load resize", function(){
+       var container = $(".gallary-box");
+        
+        container.masonry({
+            columnWidth: '.gallary-img',
+            itemSelector: '.gallary-img',
+            isFitWidth: true
+        });
+    });
+    
+    
+    
+    /*$(document).on("load resize", function(){
+          if($(window).width() <= 1228){
+            var $container = $('.gallary-box');
+        // Инициализация
+        $container.masonry({
+          columnWidth: '.gallary-img',
+          itemSelector: '.gallary-img'
+
+        }); 
+    }  
+    }) */
+    
+    
+//});
