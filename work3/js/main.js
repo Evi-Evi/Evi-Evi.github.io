d1293cabaaf667172bd5fa3fe4fd3d66ba3a3877
Evi-ava <dedasertyk.fvhg@gmail.com>
2018-08-14 13:05:55 +0400

    sdfsdfew323232

--- work3/js/main.js
+++ work3/js/main.js
@@ -1,31 +1,40 @@
-/*$(document).ready(function(){
-    
-   
-    
-    
-    $(window).on("load resize", function(){
-       var container = $(".gallary-box");
-        
-        container.masonry({
-            columnWidth: '.gallary-img',
-            itemSelector: '.gallary-img',
-            isFitWidth: true
-        });
-    });
-    
-    
-    
-    /*$(document).on("load resize", function(){
-          if($(window).width() <= 1228){
-            var $container = $('.gallary-box');
-        // Инициализация
-        $container.masonry({
-          columnWidth: '.gallary-img',
-          itemSelector: '.gallary-img'
+$(document).ready(function(){
 
-        }); 
-    }  
-    }) */
-    
-    
-//});
+var path = "https://www.yandex.ru/";
+    $("form").submit(function (e) {
+        e.preventDefault();
+
+        // в эту переменную впишише путь перенаправления
+        var path = "https://www.yandex.ru";
+
+        var check = $(this).find(".checkbox").prop("checked");
+
+        if(check && $(".first-input").val() !== "" && $(".last-input").val() !== ""){
+            var msg  = $(this).serialize();
+            $.ajax({
+                type: 'POST',
+                url: 'handler.php',
+                data: msg,
+                success: function(data) {
+                    if(data == "1"){
+                        document.location.href = path;
+                    }
+                },
+                error:  function(xhr, str){
+                    alert('Возникла ошибка');
+                }
+            });
+
+            
+        }
+
+
+
+    });
+
+    $(".last-input").on("keypress", function(key) {
+        if (key.charCode < 48 || key.charCode > 57)
+            return false;
+    });
+
+});
