dba53fc8578299821867b176207c38b57a06c2df
Evi-ava <dedasertyk.fvhg@gmail.com>
2018-08-28 21:09:16 +0400

    dfsv

--- work/js/main.js
+++ work/js/main.js
@@ -1,4 +1,57 @@
 $(document).ready(function(){
+    
+    var menuOffset =  $(".over-menu").offset().top;
+    
+    $(window).scroll(function(){
+        
+        var menu = $(".over-menu");
+        
+        var headerMid = $(".header-mid");
+      //  var menuHeight = menu.innerHeight();
+        
+        if($(this).scrollTop() > menuOffset){
+           menu.css({
+               position: "fixed",
+               top: "0",
+               left: "0",
+               background: "#fff",
+               width: "100%",
+               zIndex: "100"
+           });
+           
+           headerMid.css("margin-top", menu.innerHeight() + "px");
+        }else{
+            menu.css({
+                position: "static"
+            });
+            headerMid.css("margin-top", "0");
+        }
+    });
+    
+    $(".touch-menu").click(function(){
+        $(".menu").slideToggle(1000);
+    });
+    
+    $(window).resize(function(){
+        $(".menu").removeAttr("style");
+    });
+    
+    
+   /* $(".product-block").hover(function(){
+        $(this).find("img").hide(0);
+    }); */
+});
+
+
+
+
+
+
+
+
+
+
+/*$(document).ready(function(){
 
 var path = "https://www.yandex.ru/";
     $("form").submit(function (e) {
@@ -37,4 +90,4 @@ var path = "https://www.yandex.ru/";
             return false;
     });
 
-});
+}); */
