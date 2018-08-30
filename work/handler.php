ee419d42c45a1ca608e15454ba5861d98b8ae335
Evi-ava <dedasertyk.fvhg@gmail.com>
2018-08-30 23:29:19 +0400

    sdggg

--- work/handler.php
+++ work/handler.php
@@ -2,13 +2,56 @@
     //в эту переменную необходимо вписать ваш email
     $mail = "example@mail.ru";
 
-    if(!empty($_POST["name"]) && !empty($_POST["phone"])){
+        if(!empty($_POST["degustation"])){
+           $name = "Дегустационный набор";
+           $result = mail($mail, $name, "Номер: " . $_POST["degustation"]);
+            echo "1";
+        }
 
-        $name = htmlentities($_POST["name"]);
-        $phone = htmlentities($_POST["phone"]);
+        if(!empty($_POST["number"]) && !empty($_POST["email"])){
+            $number = htmlentities($_POST["number"]);
+            $email = htmlentities($_POST["email"]);
 
-        $result = mail($mail, $name, "Номер: " . $phone);
+            $name = "Купить чай";
 
-        echo $result;
-    }
+            $result = mail($mail, $name, "Номер: " . $number . " Почта: " . $email); 
+            
+            echo "1";
+        }
+
+        if(!empty($_POST["lid"])){
+            
+            $name = "заказать звонок";
+            
+            $result = mail($mail, $name, "Номер: " . $_POST["lid"]);
+            echo "1";
+        }
+
+        if(!empty($_POST["emailpartner"])){
+            $name = "получить каталог с ценами и скидками";
+
+            $result = mail($mail, $name, "Почта: " . $_POST["emailpartner"]);
+
+            echo "1";
+        }
+
+        if(!empty($_POST["partner"])){
+            $name = "Скачать условия партнёрства";
+
+            $result = mail($mail, $name, "Почта: " . $_POST["partner"]);
+
+            echo "1";
+        }
+
+        if(!empty($_POST["exit"])){
+            $name = "Получить книгу";
+
+            $result = mail($mail, $name, "Почта: " . $_POST["exit"]);
+
+            echo "1";
+        }
+
+        
+        
+    
 ?>
--- work/js/main.js
+++ work/js/main.js
@@ -1,55 +1,52 @@
-$(document).ready(function(){
-    
-    var menuOffset =  $(".over-menu").offset().top;
-    
-    $(window).scroll(function(){
-        
+$(document).ready(function() {
+
+    var menuOffset = $(".over-menu").offset().top;
+
+    $(window).scroll(function() {
+
         var menu = $(".over-menu");
-        
+
         var headerMid = $(".header-mid");
-      //  var menuHeight = menu.innerHeight();
-        
-        if($(this).scrollTop() > menuOffset){
-           menu.css({
-               position: "fixed",
-               top: "0",
-               left: "0",
-               background: "#fff",
-               width: "100%",
-               zIndex: "100"
-           });
-           
-           headerMid.css("margin-top", menu.innerHeight() + "px");
-        }else{
+        //  var menuHeight = menu.innerHeight();
+
+        if ($(this).scrollTop() > menuOffset) {
+            menu.css({
+                position: "fixed",
+                top: "0",
+                left: "0",
+                background: "#fff",
+                width: "100%",
+                zIndex: "100"
+            });
+
+            headerMid.css("margin-top", menu.innerHeight() + "px");
+        } else {
             menu.css({
                 position: "static"
             });
             headerMid.css("margin-top", "0");
         }
     });
-    
-    $(".touch-menu").click(function(){
+
+    $(".touch-menu").click(function() {
         $(".menu").slideToggle(1000);
     });
-    
-    $(window).resize(function(){
+
+    $(window).resize(function() {
         $(".menu").removeAttr("style");
     });
-    
-    
-  $(".reward-box").slick({
-      slidesToShow: 3,
-      adaptiveHeight: true,
-      responsive: [
-          {
-          breakpoint: 1200,
-          settings: {
-            infinite: true,
-            arrows: false,
-              slidesToShow: 2
+
+    $(".reward-box").slick({
+        slidesToShow: 3,
+        adaptiveHeight: true,
+        responsive: [{
+            breakpoint: 1200,
+            settings: {
+                infinite: true,
+                arrows: false,
+                slidesToShow: 2
             }
         },
-          
         {
             breakpoint: 800,
             settings: {
@@ -57,75 +54,243 @@ $(document).ready(function(){
                 arrows: false,
                 slidesToShow: 1,
             }
-        }
-      ]
-  });
-    
- $(".production-slider").slick({
-     
-     fade: true,
-     arrows: false,
-     dots: true,
- });
-    
-//плавный скролл
-    
-    $(".menu a").click(function(e){
-        e.preventDefault();
-        
-        var path = $(this).attr("href");
-        
-        $("body, html").animate({scrollTop: $(path).offset().top - $(".over-menu").innerHeight() - 20}, 1000);
+        }]
     });
-    
+
+    $(".production-slider").slick({
+
+        fade: true,
+        arrows: false,
+        dots: true,
+    });
+
+    $(".gallery-box").slick();
+
+    $(".gallary-box").find("img").hover(function(e) {// e.preventDefault;
+    })
+
+    //плавный скролл
+
+    $(".menu a").click(function(e) {
+        e.preventDefault();
+
+        var path = $(this).attr("href");
+
+        $("body, html").animate({
+            scrollTop: $(path).offset().top - $(".over-menu").innerHeight() - 20
+        }, 1000);
+    });
+
 });
 
+$(".reward-block").click(function() {
 
+    if ($(window).width() > 1200) {
+        $(".gallery").show(0);
+        $("body").css({
+            overflow: "hidden"
+        })
+    }
 
+});
 
+$(".ex").click(function() {
 
+    $(".gallery").hide(0);
+    $("body").css({
+        overflow: "auto",
+    })
+});
 
+$(".js-call").click(function(e) {
+    e.preventDefault();
 
+    $(".modal-js").fadeIn(300);
+    $(".modal-js").siblings(".overlay").fadeIn(300);
+});
 
+$(".js-close").click(function(e) {
+    e.preventDefault();
 
+    $(this).parent(".modal-js").fadeOut(300);
+    $(".modal-js").siblings(".overlay").fadeOut(300);
+})
 
-/*$(document).ready(function(){
+$(".btn-bar_close").click(function() {
+    $(this).closest(".modal").fadeOut(300);
+    $(".overlay").fadeOut(300);
+});
 
-var path = "https://www.yandex.ru/";
-    $("form").submit(function (e) {
-        e.preventDefault();
+$(".js-buy").click(function(e) {
+    //e.preventDefault();
 
-        // в эту переменную впишише путь перенаправления
-        var path = "https://www.yandex.ru";
+    $(".modal-js-2").fadeIn(300);
+    $(".modal-js-2").siblings(".overlay").fadeIn(300);
+});
 
-        var check = $(this).find(".checkbox").prop("checked");
+$(".js-close").click(function(e) {
+    e.preventDefault();
 
-        if(check && $(".first-input").val() !== "" && $(".last-input").val() !== ""){
-            var msg  = $(this).serialize();
-            $.ajax({
-                type: 'POST',
-                url: 'handler.php',
-                data: msg,
-                success: function(data) {
-                    if(data == "1"){
-                        document.location.href = path;
-                    }
-                },
-                error:  function(xhr, str){
-                    alert('Возникла ошибка');
-                }
-            });
+    $(this).parent(".modal-js-2").fadeOut(300);
+    $(".modal-js-2").siblings(".overlay").fadeOut(300);
+})
 
-            
+$(".js-partner").click(function(e) {
+    e.preventDefault();
+
+    $(".modal-js-3").fadeIn(300);
+    $(".modal-js-3").siblings(".overlay").fadeIn(300);
+});
+
+$(".js-close").click(function(e) {
+    e.preventDefault();
+
+    $(this).parent(".modal-js-3").fadeOut(300);
+    $(".modal-js-3").siblings(".overlay").fadeOut(300);
+})
+
+$(".js-sale").click(function(e) {
+    e.preventDefault();
+
+    $(".modal-js-4").fadeIn(300);
+    $(".modal-js-4").siblings(".overlay").fadeIn(300);
+});
+
+$(".js-close").click(function(e) {
+    e.preventDefault();
+
+    $(this).parent(".modal-js-4").fadeOut(300);
+    $(".modal-js-4").siblings(".overlay").fadeOut(300);
+})
+
+$(".header-btn").click(function(e) {
+
+    $(".modal-js-5").fadeIn(300);
+    $(".modal-js-5").siblings(".overlay").fadeIn(300);
+});
+
+$(".modal__btn-bar").click(function() {
+    $(this).closest(".modal").fadeOut(300);
+
+    $(".modal-js-5").fadeIn(300);
+})
+
+$(".js-close").click(function(e) {
+    e.preventDefault();
+
+    $(this).parent(".modal-js-5").fadeOut(300);
+    $(".modal-js-5").siblings(".overlay").fadeOut(300);
+})
+
+//buy
+
+$(".modal-js-2 form").submit(function(e) {
+    e.preventDefault();
+
+    var msg = $(this).serialize();
+
+    $.ajax({
+        type: 'POST',
+        url: 'handler.php',
+        data: msg,
+        success: function(data) {},
+        error: function(xhr, str) {
+            alert('Возникла ошибка');
         }
-
-
-
     });
+});
 
-    $(".last-input").on("keypress", function(key) {
-        if (key.charCode < 48 || key.charCode > 57)
-            return false;
+//dugust
+
+$(".header-form").submit(function(e) {
+    e.preventDefault();
+
+    var msg = $(this).serialize();
+
+    $.ajax({
+        type: 'POST',
+        url: 'handler.php',
+        data: msg,
+        success: function(data) {
+            if (data == "1") {
+                $(".modal-js-5").fadeIn(300);
+                $(".modal-js-5").siblings(".overlay").fadeIn(300);
+            }
+
+        },
+        error: function(xhr, str) {
+            alert('Возникла ошибка');
+        }
     });
+});
 
-}); */
+//заказ звонка
+
+$(".modal-js form").submit(function(e) {
+    e.preventDefault();
+
+    var msg = $(this).serialize();
+
+    $.ajax({
+        type: 'POST',
+        url: 'handler.php',
+        data: msg,
+        success: function(data) {
+
+            $(".modal-js").fadeIn(300);
+            $(".modal-js").siblings(".overlay").fadeIn(300);
+
+        },
+        error: function(xhr, str) {
+            alert('Возникла ошибка');
+        }
+    });
+});
+
+$(".modal-js-4 form").submit(function(e) {
+    e.preventDefault();
+
+    var msg = $(this).serialize();
+
+    $.ajax({
+        type: 'POST',
+        url: 'handler.php',
+        data: msg,
+        success: function(data) {},
+        error: function(xhr, str) {
+            alert('Возникла ошибка');
+        }
+    });
+});
+
+$(".modal-js-3 form").submit(function(e) {
+    e.preventDefault();
+
+    var msg = $(this).serialize();
+
+    $.ajax({
+        type: 'POST',
+        url: 'handler.php',
+        data: msg,
+        success: function(data) {},
+        error: function(xhr, str) {
+            alert('Возникла ошибка');
+        }
+    });
+});
+
+$(".modal-js-6 form").submit(function(e) {
+    e.preventDefault();
+
+    var msg = $(this).serialize();
+
+    $.ajax({
+        type: 'POST',
+        url: 'handler.php',
+        data: msg,
+        success: function(data) {},
+        error: function(xhr, str) {
+            alert('Возникла ошибка');
+        }
+    });
+});
